9fd647fbfba2d2a667a678775d1800724e4b7c19
BobaBot253 <253programming@gmail.com>
2024-02-03 13:19:15 -0800

    add openCV AprilTag pipeline

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,7 +4,24 @@
 
 package frc.robot;
 
+import java.util.ArrayList;
+
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+import edu.wpi.first.apriltag.AprilTagDetection;
+import edu.wpi.first.apriltag.AprilTagDetector;
+import edu.wpi.first.apriltag.AprilTagPoseEstimator;
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.CvSink;
+import edu.wpi.first.cscore.CvSource;
+import edu.wpi.first.cscore.UsbCamera;
+import edu.wpi.first.math.estimator.PoseEstimator;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -25,11 +42,122 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+    Thread visionThread = new Thread(() -> apriltagVisionThreadProc());
+    visionThread.setDaemon(true);
+    visionThread.start();
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
-    m_robotContainer = new RobotContainer();
+
+    // PhotonVision testing
+    // m_robotContainer = new RobotContainer();
+    // CameraServer.startAutomaticCapture();
+    // CvSink cvSink = CameraServer.getVideo();
+
+    // // set up AprilTag detector
+    // AprilTagDetector detector = new AprilTagDetector();
+    // AprilTagDetector.Config config = new AprilTagDetector.Config();
+    // // set config parameters, e.g. config.blah = 5;
+    // detector.setConfig(config);
+    // detector.addFamily("tag16h5");
+
+    // // Set up Pose Estimator
+    // AprilTagPoseEstimator.Config poseEstConfig = new AprilTagPoseEstimator.Config(
+    //   kDefaultPeriod, 
+    //   kDefaultPeriod, 
+    //   kDefaultPeriod, 
+    //   kDefaultPeriod, 
+    //   kDefaultPeriod
+    //   );
+    // AprilTagPoseEstimator estimator = new AprilTagPoseEstimator(poseEstConfig);
+
+    // Mat mat = new Mat();
+    // Mat graymat = new Mat();
+
+    // while (!Thread.interrupted()) {
+    //   // grab image from camera
+    //   long time = cvSink.grabFrame(mat);
+    //   if (time == 0) {
+    //     continue;  // error getting image
+    //   }
+
+    //   // convert image to grayscale
+    //   Imgproc.cvtColor(mat, graymat, Imgproc.COLOR_BGR2GRAY);
+      
+    //   // run detection
+    //   for (AprilTagDetection detection : detector.detect(graymat)) {
+    //     // filter by property
+
+    //     // run pose estimator
+    //     Transform3d pose = PoseEstimator.estimate(detection);
+    //   }
+    // }
   }
 
+  void apriltagVisionThreadProc() {
+    AprilTagDetector detector = new AprilTagDetector();
+    detector.addFamily("tag16h5", 0);
+  
+    // Get the UsbCamera from CameraServer
+    UsbCamera camera = CameraServer.startAutomaticCapture();
+    // Set the resolution
+    camera.setResolution(640, 480);
+
+    // Get a CvSink. This will capture Mats from the camera
+    CvSink cvSink = CameraServer.getVideo();
+    // Setup a CvSource. This will send images back to the Dashboard
+    CvSource outputStream = CameraServer.putVideo("detect", 640, 480);
+
+    // Mats are very memory expensive. Lets reuse this Mat.
+    Mat mat = new Mat();
+    Mat grayMat = new Mat();
+    ArrayList<Integer> tags = new ArrayList<>();
+
+    //
+    Scalar outlineColor = new Scalar(0, 255, 0);
+    Scalar xColor = new Scalar(0, 0, 255);
+
+    // This cannot be 'true'. The program will never exit if it is. This
+    // lets the robot stop this thread when restarting robot code or
+    // deploying.
+    while (!Thread.interrupted()) {
+      // Tell the CvSink to grab a frame from the camera and put it
+      // in the source mat.  If there is an error notify the output.
+      if (cvSink.grabFrame(mat) == 0) {
+        // Send the output the error.
+        outputStream.notifyError(cvSink.getError());
+        // skip the rest of the current iteration
+        continue;
+      }
+
+      Imgproc.cvtColor(mat, grayMat, Imgproc.COLOR_RGB2GRAY);
+
+      AprilTagDetection[] detections = detector.detect(grayMat);
+      tags.clear();
+      for (AprilTagDetection detection : detections) {
+        tags.add(detection.getId());
+
+        for (var i = 0; i <= 3; i++) {
+          var j = (i + 1) % 4;
+          var pt1 = new Point(detection.getCornerX(i), detection.getCornerY(i));
+          var pt2 = new Point(detection.getCornerX(j), detection.getCornerY(j));
+          Imgproc.line(mat, pt1, pt2, outlineColor, 2);
+        }
+
+        var cx = detection.getCenterX();
+        var cy = detection.getCenterY();
+        var ll = 10;
+        Imgproc.line(mat, new Point(cx - ll, cy), new Point(cx + ll, cy), xColor, 2);
+        Imgproc.line(mat, new Point(cx, cy - ll), new Point(cx, cy + ll), xColor, 2);
+        Imgproc.putText(mat, Integer.toString(detection.getId()), new Point (cx + ll, cy), Imgproc.FONT_HERSHEY_SIMPLEX, 1, xColor, 3);
+      }
+
+      SmartDashboard.putString("tag", tags.toString());
+      // Give the output stream a new image to display
+      outputStream.putFrame(mat);
+    }
+
+    detector.close();
+  }
   /**
    * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
    * that you want ran during disabled, autonomous, teleoperated and test.
